ac16ba3cec8ec0737062926d7c8ae821f9cf566f
Davincode <luchao@LuChaos-MacBook-Pro.local>
2015-04-22 16:00:22 -0700

    tiny change

--- HIve/Hive.py
+++ HIve/Hive.py
@@ -1,53 +1,28 @@
-#!/usr/bin/env python
-
-from hive_service import ThriftHive
-from hive_service.ttypes import HiveServerException
-from thrift import Thrift
-from thrift.transport import TSocket
-from thrift.transport import TTransport
-from thrift.protocol import TBinaryProtocol
+import pyhs2
 
 
 class Hive():
 
     def __init__(self, select_sql):
         self.query = open(select_sql, 'r')
+        self.num_query = 3
 
     def run(self):
-        try:
-            transport = TSocket.TSocket('localhost', 10000)
-            transport = TTransport.TBufferedTransport(transport)
-            protocol = TBinaryProtocol.TBinaryProtocol(transport)
 
-            client = ThriftHive.Client(protocol)
-            transport.open()
+        conn = pyhs2.connect(host='localhost',
+                             port=10000,
+                             authMechanism="PLAIN",
+                             user='root',
+                             password='test',
+                             database='default')
 
-            client.execute(self.query.readline())
-            row = client.fetchOne()
-            print "Scan:\n"
-            for r in row:
-                print r
-            print "Finish the first query\n"
-
-            client.execute(self.query.readline())
-            row = client.fetchAll()
-            print "Join:\n"
-            for r in row:
-                print r
-            print "Finish the second query\n"
-
-            client.execute(self.query.readline())
-            row = client.fetchAll()
-            print "Aggregation:\n"
-            for r in row:
-                print r
-            print "Finish the third query\n"
-
-            transport.close()
-
-        except Thrift.TException, tx:
-            print '%s' % tx.message
-
+        cur = conn.cursor()
+        for index in range(self.num_query):
+            cur.execute(self.query.readline())
+            for i in cur.fetch():
+                print i
+        cur.close()
+        conn.close()
 
 if __name__ == '__main__':
     hive = Hive("hive.sql")
